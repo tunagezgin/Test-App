86dfa098ab165a5dba4ac731cd5121684c2b9328
tunagezgin <tunagezgin04@gmail.com>
2025-09-04 15:13:55 +0300

    Update start.sh

--- scripts/scripts/start.sh
+++ scripts/scripts/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-# Aşağıdaki ECR URI'yi AWS Console > ECR'den alıp buraya yapıştır.
-ECR_URI="AWS_ACCOUNT_ID.dkr.ecr.AWS_REGION.amazonaws.com/REPO_NAME:latest"
+# ECR URI'yi kendi hesabına göre düzenle
+ECR_URI="AWS_ACCOUNT_ID.dkr.ecr.AWS_REGION.amazonaws.com/Test-App:latest"
 
 # Pull & run
 docker pull $ECR_URI
-docker run -d --name app --restart unless-stopped -p 80:80 $ECR_URI
+docker run -d --name Test-App --restart unless-stopped -p 80:80 $ECR_URI
